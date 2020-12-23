2912c352dabbb8adaca284dcdec5d5f58fb44507
Patryk Laskowski <patryk.jacek.laskowski@gmail.com>
2020-12-23 23:51:17 +0100

    Update generate_train.py

--- generate_train.py
+++ generate_train.py
@@ -1,16 +1,19 @@
 # generate_train.py
 
-import os
- 
+import os 
+
+train_folder_name = 'train_zebra_train'
+
 image_files = []
-os.chdir(os.path.join("data", "obj"))
+os.chdir(os.path.join("data", train_folder_name))
 for filename in os.listdir(os.getcwd()):
     if filename.endswith(".jpg"):
-        image_files.append("data/obj/" + filename)
-os.chdir("..")
-with open("train.txt", "w") as outfile:
+        image_files.append(f"data/{train_folder_name}/" + filename)
+
+os.chdir("..") # /<base dir>/data
+with open("train.txt", "w") as file:
     for image in image_files:
-        outfile.write(image)
-        outfile.write("\n")
-    outfile.close()
-os.chdir("..")
+        file.write(image)
+        file.write("\n")
+
+os.chdir("..") # /<base dir>
